a62b832922f68a3044ec840bb568a2bf2424d5a0
Md. Rasel Hossain <59763029+Rasel66@users.noreply.github.com>
2022-09-05 11:05:49 +0600

    Add files via upload

---
+++ Janmansh and Games.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--)
+    {
+        int x,y;
+        cin>>x>>y;
+        if(x%2==0)
+        {
+            if(y%2==0)
+            {
+                cout<<"Janmansh"<<endl;
+            }
+            else
+            {
+                cout<<"Jay"<<endl;
+            }
+        }
+        else
+        {
+            if(y%2==0)
+            {
+                cout<<"Jay"<<endl;
+            }
+            else
+            {
+                cout<<"Janmansh"<<endl;
+            }
+        }
+    }
+}
